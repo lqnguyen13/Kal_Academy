05727ed125dffb1bcdbc478714f9117e6e22b59c
arpitaojha1983 <abarpita@yahoo.com>
2018-06-23 13:57:46 -0700

    Initial setup
    Created models, song control

---
+++ MusicLibrary/Models/Album.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace MusicLibrary.Models
+{
+    class Album
+    {
+        public string AlbumTitle { get; set; }
+        public string AlbumArtist { get; set; }
+        public BitmapImage AlbumImage { get; set; }
+    }
+}
---
+++ MusicLibrary/Models/song.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace MusicLibrary.Models
+{
+    class song
+    {
+        public string SongTitle { get; set; }
+        public BitmapImage SongImage { get; set; }
+    }
+}
