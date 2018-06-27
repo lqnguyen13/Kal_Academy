fac87500e8a1a79212310e4e95c263a86a703a7a
arpitaojha1983 <abarpita@yahoo.com>
2018-06-27 00:02:30 -0700

    Added song class, make Song UI and added play music , stop music
    Added song class woth properrties
    Added a method songmanager to add songs in the list , and list of songs will display in the mainpage.xaml throiugh data binding. Also added play and stop music. If you click on each row , you will get the message that "you selected ** song".

--- MusicLibrary/MainPage.xaml.cs
+++ MusicLibrary/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using MusicLibrary.Models;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +23,61 @@ namespace MusicLibrary
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        private List<Song> Songs;
+
+        public int Assets { get; private set; }
+
         public MainPage()
         {
             this.InitializeComponent();
+            Songs = SongManager.GetSong();
+        }
+
+
+        public void playSoundButton_Click(object sender, RoutedEventArgs e)
+        {
+            myMediaElement.Play();
+        }
+
+        private void StopSoundButton_Click(object sender, RoutedEventArgs e)
+        {
+            myMediaElement.Stop();
+        }
+
+        private void PauseSoundButton_Click(object sender, RoutedEventArgs e)
+        {
+            myMediaElement.Pause();
+        }
+
+        public void SongGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+
+            var song = (Song)e.ClickedItem;
+            ResultTextBlock.Text = "You Selected" + song.SongTitle;
+            // song.Play_Button_Click();
+
+        }
+
+
+
+
+        public void Play_Button_Click(object sender, RoutedEventArgs e)
+        {
+            //  if (song.SongTitle == "Roar")
+
+
+
+        }
+
+
+        private void Stop_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            myMediaElement.Stop();
         }
     }
+
+
 }
--- MusicLibrary/Models/song.cs
+++ MusicLibrary/Models/song.cs
@@ -3,14 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Windows.UI.Xaml.Media.Imaging;
 
 namespace MusicLibrary.Models
 {
-    class Song
+    public class Song
     {
+        public int SongID { get; set; }
         public string SongTitle { get; set; }
-        public string SongFileName { get; set; }
-        public BitmapImage SongImage { get; set; }
+        public string SongArtist { get; set; }
+        public string SongImage { get; set; }
+        public string AudioFile { get; set; }
+
     }
+
+
+
+    public class SongManager
+    {
+        public static List<Song> GetSong()
+        {
+            var songs = new List<Song>();
+
+            songs.Add(new Song { SongID = 1, SongTitle = "Roar", SongArtist = "Kathy Perry", SongImage = "Assets/lata.jpg" });
+            songs.Add(new Song { SongID = 2, SongTitle = "DarkHorse", SongArtist = "Britney Spherse", SongImage = "Assets/lata.jpg" });
+            songs.Add(new Song { SongID = 3, SongTitle = "Bollywood", SongArtist = "Latha Mangeshwar", SongImage = "Assets/katyperry.jpg" });
+            return songs;
+        }
+    }
+
+
+
+
+
+
+
+
+
 }
