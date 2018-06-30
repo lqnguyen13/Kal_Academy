167a8272134d414438519ff6f4d6519078e18071
Leslie Nguyen <leslie.qn@gmail.com>
2018-06-30 00:05:07 -0700

    Added main page UI structure
    Renamed Song.xaml to SongControl.xaml; Added all elements to UI and connected read/write functionality

--- MusicLibrary/MainPage.xaml.cs
+++ MusicLibrary/MainPage.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using MusicLibrary.Models;
+using Windows.UI.Xaml.Media.Imaging;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -24,23 +25,44 @@ namespace MusicLibrary
     public sealed partial class MainPage : Page
     {
 
-        private List<Song> Songs;
+        //private List<Song> Songs;
 
-        public int Assets { get; private set; }
+        //public int Assets { get; private set; }
 
         public MainPage()
         {
             this.InitializeComponent();
-            Songs = SongManager.GetSong();
+            //Songs = SongManager.GetSong();
         }
 
         protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
-            Songs = await SongManager.ReadSongsFromFile();
+            //Songs = await SongManager.ReadSongsFromFile();
+            this.DataContext = await Song.GetSongs();
         }
 
-        public void playSoundButton_Click(object sender, RoutedEventArgs e)
+        private void SongsGrid_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var song = (Song)e.ClickedItem;
+            BitmapImage image = new BitmapImage(new Uri("ms-appx:///" + song.SongImage, UriKind.RelativeOrAbsolute));
+            this.SelectedSongTitle.Text = song.SongTitle;
+            this.SelectedSongArtist.Text = song.SongArtist;
+            this.SelectedSongImage.Source = image;
+            this.myMediaElement.Source = new Uri("ms-appx:///" + song.AudioFile, UriKind.RelativeOrAbsolute);
+        }
+
+        private void AddRemove_Click(object sender, RoutedEventArgs e)
+        {
+            // ENTER CODE/FUNCTION CALL HERE TO NAVIGATE TO A SEPARATE PAGE TO ADD A SONG
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            // ENTER CODE/FUNCTION CALL HERE TO SEARCH FOR SONG INDICATED IN SEARCH BOX
+        }
+
+        public void PlaySoundButton_Click(object sender, RoutedEventArgs e)
         {
             myMediaElement.Play();
         }
@@ -55,33 +77,33 @@ namespace MusicLibrary
             myMediaElement.Pause();
         }
 
-        public void SongGridView_ItemClick(object sender, ItemClickEventArgs e)
-        {
+        //public void SongGridView_ItemClick(object sender, ItemClickEventArgs e)
+        //{
 
 
-            var song = (Song)e.ClickedItem;
-            ResultTextBlock.Text = "You Selected" + song.SongTitle;
-            // song.Play_Button_Click();
+        //    var song = (Song)e.ClickedItem;
+        //    ResultTextBlock.Text = "You Selected" + song.SongTitle;
+        //    // song.Play_Button_Click();
 
-        }
-
-
-
-
-        public void Play_Button_Click(object sender, RoutedEventArgs e)
-        {
-            //  if (song.SongTitle == "Roar")
+        //}
 
 
 
-        }
+
+        //public void Play_Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    //  if (song.SongTitle == "Roar")
 
 
-        private void Stop_Button_Click(object sender, RoutedEventArgs e)
-        {
 
-            myMediaElement.Stop();
-        }
+        //}
+
+
+        //private void Stop_Button_Click(object sender, RoutedEventArgs e)
+        //{
+
+        //    myMediaElement.Stop();
+        //}
     }
 
 
--- MusicLibrary/Models/song.cs
+++ MusicLibrary/Models/song.cs
@@ -9,34 +9,16 @@ namespace MusicLibrary.Models
 
     public class Song
     {
+
+        const string TEXT_FILE_NAME = "SongsTextfile.txt";
+
         public int SongID { get; set; }
         public string SongTitle { get; set; }
         public string SongArtist { get; set; }
         public string SongImage { get; set; }
         public string AudioFile { get; set; }
 
-    }
-
-
-
-    public class SongManager
-    {
-        const string TEXT_FILE_NAME = "SongsTextfile.txt";
-
-        public static List<Song> GetSong()
-        {
-            var songs = new List<Song>();
-
-            songs.Add(new Song { SongID = 1, SongTitle = "Roar", SongArtist = "Kathy Perry", SongImage = "Assets/lata.jpg" });
-            songs.Add(new Song { SongID = 2, SongTitle = "DarkHorse", SongArtist = "Britney Spherse", SongImage = "Assets/lata.jpg" });
-            songs.Add(new Song { SongID = 3, SongTitle = "Bollywood", SongArtist = "Latha Mangeshwar", SongImage = "Assets/katyperry.jpg" });
-            return songs;
-        }
-
-        // read songs from file
-        // return list of Song objects
-    
-        public static async  Task<List<Song>> ReadSongsFromFile()
+        public async static Task<ICollection<Song>> GetSongs()
         {
             var songs = new List<Song>();
             var songList = await FileHelper.ReadTextFile(TEXT_FILE_NAME);
@@ -49,17 +31,59 @@ namespace MusicLibrary.Models
                     SongTitle = songParts[1],
                     SongArtist = songParts[2],
                     SongImage = songParts[3],
+                    AudioFile = songParts[4]
                 };
                 songs.Add(newSong);
             }
             return songs;
         }
 
-
-
+        public async static void WriteSong(Song song)
+        {
+            var songData = $"{song.SongID}|{song.SongTitle}|{song.SongArtist}|{song.SongImage}|{song.AudioFile}";
+            await FileHelper.WriteOneLine(TEXT_FILE_NAME, songData);
+        }
 
 
     }
+    //public class SongManager
+    //{
+    //    const string TEXT_FILE_NAME = "SongsTextfile.txt";
+
+    //    public static List<Song> GetSong()
+    //    {
+    //        var songs = new List<Song>();
+
+    //        songs.Add(new Song { SongID = 1, SongTitle = "Roar", SongArtist = "Kathy Perry", SongImage = "Assets/lata.jpg" });
+    //        songs.Add(new Song { SongID = 2, SongTitle = "DarkHorse", SongArtist = "Britney Spherse", SongImage = "Assets/lata.jpg" });
+    //        songs.Add(new Song { SongID = 3, SongTitle = "Bollywood", SongArtist = "Latha Mangeshwar", SongImage = "Assets/katyperry.jpg" });
+    //        return songs;
+    //    }
+
+    //    // read songs from file
+    //    // return list of Song objects
+    
+    //    public static async  Task<List<Song>> ReadSongsFromFile()
+    //    {
+    //        var songs = new List<Song>();
+    //        var songList = await FileHelper.ReadTextFile(TEXT_FILE_NAME);
+    //        foreach (var aSong in songList)
+    //        {
+    //            var songParts = aSong.Split('|');
+    //            var newSong = new Song
+    //            {
+    //                SongID = int.Parse(songParts[0]),
+    //                SongTitle = songParts[1],
+    //                SongArtist = songParts[2],
+    //                SongImage = songParts[3],
+    //                AudioFile = songParts[4]
+    //            };
+    //            songs.Add(newSong);
+    //        }
+    //        return songs;
+    //    }
+
+    //}
 
 
 
