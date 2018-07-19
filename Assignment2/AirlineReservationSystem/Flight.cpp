00040a6b72860e47c96fb8286f6a34a52d449367
arpitaojha1983 <abarpita@yahoo.com>
2018-07-18 23:29:25 -0700

    added flight.h and flight.cpp

--- Assignment2/AirlineReservationSystem/Flight.cpp
+++ Assignment2/AirlineReservationSystem/Flight.cpp
@@ -1,8 +1,98 @@
 #include "stdafx.h"
 #include "Flight.h"
+#include <iostream>
+
 
 using namespace std;
 
-namespace AirlineReservationSystem {
+namespace AirlineReservationSystem
+{
+	Flight::Flight(const string& depart, const string& arrive, const int flightno)
+		: mDepartLoc(depart), mArriveLoc(arrive), mFlightNum(flightno) {}
 
-}
+
+
+	void Flight::setDepartLoc(const string& depart) {
+		mDepartLoc = depart;
+	}
+
+	const string& Flight::getDepartLoc() const {
+		return mDepartLoc;
+	}
+
+	void Flight::setArriveLoc(const string& arrive) {
+		mArriveLoc = arrive;
+	}
+
+	const string& Flight::getArriveLoc() const {
+		return mArriveLoc;
+	}
+
+	void Flight::setDepartTime(const std::string & departtime)
+	{
+		mDepartTime = departtime;
+	}
+
+	const std::string & Flight::getDepartTime() const
+	{
+		return mDepartTime;
+	}
+
+	void Flight::setArriveTime(const std::string & arrivetime)
+	{
+		mArriveTime = arrivetime;
+	}
+
+	const std::string & Flight::getArriveTime() const
+	{
+		return mArriveTime;
+	}
+
+	void Flight::setFlightDuration(float duration)
+	{
+		mFlightDur = duration;
+	}
+
+	float Flight::getFlightDuration() const
+	{
+		return mFlightDur;
+	}
+
+	void Flight::setTotalSeat(int noofseat)
+	{
+		mTotalSeats = noofseat;
+	}
+
+	int Flight::getTotalSeat() const
+	{
+		return mTotalSeats;
+	}
+
+
+	bool Flight::isReserved() const
+	{
+		return mReserved;
+	}
+
+	void Flight::setFlightno(int flightno) {
+		mFlightNum = flightno;
+	}
+
+	int Flight::getFlightno() const {
+		return mFlightNum;
+	}
+
+
+		void Flight::displayFlight() const {
+			cout << "FlightNo: " << getFlightno() << endl;
+			cout << "Arrival Loc:  " << getArriveLoc() << endl;
+			cout << "Depart Loc: " << getDepartLoc() << endl;
+			cout << "-------------------------" << endl;
+		}
+
+
+
+
+	}
+
+
--- Assignment2/AirlineReservationSystem/Flight.h
+++ Assignment2/AirlineReservationSystem/Flight.h
@@ -19,5 +19,38 @@ namespace AirlineReservationSystem {
 	public:
 		// TODO: WRITE SETTERS/GETTERS AND METHODS IN HEADER FILE
 
+		Flight() = default;
+		Flight(const std::string& depart,
+			const std::string& arrive,
+			int flightno);
+
+	
+
+		//Getters and setters
+
+
+		bool isReserved() const;
+
+		void setFlightno(int flightno);
+		void displayFlight() const;
+		int getFlightno() const;
+
+		void setDepartLoc(const std::string& depart);
+		const std::string& getDepartLoc() const;
+
+		void setArriveLoc(const std::string& arrive);
+		const std::string& getArriveLoc() const;
+
+		void setDepartTime(const std::string& departtime);
+		const std::string& getDepartTime() const;
+
+		void setArriveTime(const std::string& arrivetime);
+		const std::string& getArriveTime() const;
+
+		void setFlightDuration(float duration);
+		float getFlightDuration() const;
+
+		void setTotalSeat(int noofseat);
+		int getTotalSeat() const;
 	};
 }
