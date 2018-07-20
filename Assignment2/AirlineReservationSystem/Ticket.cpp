b8b4239526280eb8586f914e6e4bbb6bda07daaa
Cathy Sullivan <cesullivan@gmail.com>
2018-07-19 17:37:02 -0700

    Ticket updated
    I had already done ticket.h. Now ticket.cpp is done. TicketDatase sitll needs to be workd on.

--- Assignment2/AirlineReservationSystem/Ticket.cpp
+++ Assignment2/AirlineReservationSystem/Ticket.cpp
@@ -3,6 +3,53 @@
 
 using namespace std;
 
+//int mTicketNum;
+//int mPassengerID;
+//int mFlightNum;
+//std::string mFlightDate;
+//std::string mSeat;
+
+
 namespace AirlineReservationSystem {
+
+	Ticket::Ticket(int& ticketNum, int& passengerID, int& flightNum, const std::string& flightDate, const std::string& seat) :
+		mTicketNum(ticketNum), mPassengerID(passengerID), mFlightNum(flightNum), mFlightDate(flightDate), mSeat(seat)
+	{}
+
+
+	void Ticket::setTicketNum(int ticketNum)
+    {
+		mTicketNum = ticketNum;
+	}
+
+	int Ticket::getTicketNum () const
+	{
+		return mTicketNum;
+	}
+	void Ticket::setPassengerID(int passengerID) {
+		mPassengerID = passengerID;
+	}
+	int Ticket::getPassengerID() const {
+		return mPassengerID;
+	}
+	void Ticket::setFlightNum(int flighNum) {
+		mFlightNum = flighNum;
+	}
+	int Ticket::getFlightNum() const {
+		return mFlightNum;
+	}
+	void Ticket::setFlightDate(const std::string& flightDate)
+	{
+		mFlightDate = flightDate;
+	}
+
+	void Ticket::setSeat(const std::string& seat)
+	{
+		mSeat = seat;
+	}
+	const std::string& Ticket::getSeat() const
+	{
+		return mSeat;
+	}
 
 }
--- Assignment2/AirlineReservationSystem/Ticket.h
+++ Assignment2/AirlineReservationSystem/Ticket.h
@@ -12,7 +12,24 @@ namespace AirlineReservationSystem {
 		std::string mSeat;
 
 	public:
-		// TODO: WRITE SETTERS/GETTERS AND METHODS IN HEADER FILE
+		Ticket() = default;
+		//Getters and setters
 
+		Ticket(int & mTicketNum, int & mPassengerID, int & mFlightNum, const std::string & mFlightDate, const std::string & mSeat);
+
+		void setTicketNum(int ticketNum);
+		int getTicketNum() const;
+
+		void setPassengerID(int passengerID);
+		int getPassengerID() const;
+
+		void setFlightNum(int flightNum);
+		int getFlightNum() const;
+
+		void setFlightDate(const std::string& flightDate);
+		const std::string& getFlightDate() const;
+
+		void setSeat(const std::string& seat);
+		const std::string& getSeat() const;
 	};
 }
