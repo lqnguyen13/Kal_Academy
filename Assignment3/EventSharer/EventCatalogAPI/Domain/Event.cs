a0a369657197ca895780306cbc46fc05a6d60efa
John Kariungi <john.k.gitonga@gmail.com>
2018-07-28 12:55:03 -0700

    Added domains for event, event type and location

---
+++ Assignment3/EventSharer/EventCatalogAPI/Domain/Event.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogAPI.Domain
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }
+        public DateTime EventDate { get; set; }
+        public DateTime EventTime { get; set; }
+
+        public int EventTypeId { get; set; }
+        public int LoactionId { get; set; }
+
+        public virtual EventType EventType { get; set; }
+        public virtual Location Location { get; set; }
+    }
+}
