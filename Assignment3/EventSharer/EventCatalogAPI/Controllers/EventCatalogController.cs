c08822c56f948500d29ba53097df40e57958290c
Leslie Nguyen <leslie.qn@gmail.com>
2018-07-29 09:38:11 -0700

    Added EventCatalogController
    Including necessary dependencies for constructor

---
+++ Assignment3/EventSharer/EventCatalogAPI/Controllers/EventCatalogController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EventCatalogAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace EventCatalogAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/EventCatalog")]
+    public class EventCatalogController : Controller
+    {
+        private readonly EventCatalogContext _catalogContext;
+        private readonly IOptionsSnapshot<EventCatalogSettings> _settings;
+
+        public EventCatalogController(EventCatalogContext catalogContext, IOptionsSnapshot<EventCatalogSettings> settings)
+        {
+            _catalogContext = catalogContext;
+            _settings = settings;
+        }
+
+        //TODO Add API Get, Post, Put, and Delete methods
+    }
+}
---
+++ Assignment3/EventSharer/EventCatalogAPI/EventCatalogSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogAPI
+{
+    public class EventCatalogSettings
+    {
+        public string ExternalCatalogBaseUrl { get; set; }
+    }
+}
