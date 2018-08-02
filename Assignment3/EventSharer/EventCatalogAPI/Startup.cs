4f993af7dfa0dfe6b4a3cc4db313c7e0eeecd7e4
Leslie Nguyen <leslie.qn@gmail.com>
2018-08-01 20:37:29 -0700

    Include code to dockerize

--- Assignment3/EventSharer/EventCatalogAPI/Startup.cs
+++ Assignment3/EventSharer/EventCatalogAPI/Startup.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EventCatalogAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -23,7 +25,18 @@ namespace EventCatalogAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<EventCatalogSettings>(Configuration);
+
             services.AddMvc();
+            // set up configuration string based on docker-compose
+            var server = Configuration["DatabaseServer"];
+            var database = Configuration["DatabaseName"];
+            var user = Configuration["DatabaseUser"];
+            var password = Configuration["DatabaseUserPassword"];
+            var connectionString = String.Format("Server={0};Database={1};User={2};Password={3}", server, database, user, password);
+
+            services.AddDbContext<EventCatalogContext>
+                (options => options.UseSqlServer(connectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
