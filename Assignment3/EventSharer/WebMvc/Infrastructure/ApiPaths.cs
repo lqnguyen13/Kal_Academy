70333691e7df9d4d1bb26e07163a3657e989af83
Leslie Nguyen <leslie.qn@gmail.com>
2018-08-10 21:33:38 -0700

    Add WebMvc infrastructure

---
+++ Assignment3/EventSharer/WebMvc/Infrastructure/ApiPaths.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMvc.Infrastructure
+{
+    public class ApiPaths
+    {
+        public static class EventCatalog
+        {
+            public static string GetAllEvents(string baseUri, int page, int take, int? location, int? type)
+            {
+                var filterQs = string.Empty;
+
+                if (location.HasValue || type.HasValue)
+                {
+                    var locationQs = (location.HasValue) ? location.Value.ToString() : "null";
+                    var typeQs = (type.HasValue) ? type.Value.ToString() : "null";
+                    filterQs = $"/type/{typeQs}/location/{locationQs}";
+                }
+                
+                return $"{baseUri}events{filterQs}?pageIndex={page}&pageSize={take}";
+            }
+
+            public static string GetEvent(string baseUri, int id)
+            {
+                return $"{baseUri}events/{id}";
+            }
+
+            public static string GetAllLocations(string baseUri)
+            {
+                return $"{baseUri}locations";
+            }
+
+            public static string GetAllTypes(string baseUri)
+            {
+                return $"{baseUri}eventTypes";
+            }
+        }
+    }
+}
