97b3bf689bb903bd72897450f93fb0300aabf975
Leslie Nguyen <leslie.qn@gmail.com>
2018-08-17 06:57:55 -0700

    Add identity seed data

---
+++ Assignment3/EventSharer/TokenServiceApi/Data/IdentityDbInit.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TokenServiceApi.Models;
+
+namespace TokenServiceApi.Data
+{
+    public class IdentityDbInit
+    {
+        private static UserManager<ApplicationUser> _userManager;
+
+        public static async void Initialize(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+
+            context.Database.Migrate();
+
+            if (context.Users.Any(r => r.UserName == "me@myemail.com")) return;
+
+            string user = "test@email.com";
+            string password = "Password123!";
+            await _userManager.CreateAsync(new ApplicationUser { UserName = user, Email = user, EmailConfirmed = true }, password);
+        }
+    }
+}
--- Assignment3/EventSharer/TokenServiceApi/Program.cs
+++ Assignment3/EventSharer/TokenServiceApi/Program.cs
@@ -5,8 +5,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using TokenServiceApi.Data;
+using TokenServiceApi.Models;
 
 namespace TokenServiceApi
 {
@@ -14,11 +18,29 @@ namespace TokenServiceApi
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = BuildWebHost(args);
+            var scope = host.Services.CreateScope();
+
+            var services = scope.ServiceProvider;
+
+            try
+            {
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                IdentityDbInit.Initialize(context, userManager);
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occured while seeding the AuthorizationServer database.");
+            }
+
+            host.Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .Build();
     }
 }
