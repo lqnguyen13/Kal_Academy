58f8a192f370b0a44cb573aac421d7bd27fedd0a
Leslie Nguyen <leslie.qn@gmail.com>
2018-08-29 22:54:05 -0700

    WebMvc - add cart and order models

---
+++ Assignment3/EventSharer/WebMvc/Models/CartModels/Cart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMvc.Models.CartModels
+{
+    public class Cart
+    {
+        public List<CartItem> Items { get; set; }
+        public string BuyerId { get; set; }
+
+        public decimal Total()
+        {
+            return Math.Round(Items.Sum(x => x.EventPrice * x.Quantity), 2);
+        }
+    }
+}
---
+++ Assignment3/EventSharer/WebMvc/Models/OrderModels/Order.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMvc.Models.OrderModels
+{
+    public class Order
+    {
+        [BindNever]
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal OrderTotal { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string UserName { get; set; }
+        public string BuyerId { get; set; }
+        public string StripeToken { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public List<OrderItem> OrderItems { get; } = new List<OrderItem>();
+        public string PaymentAuthCode { get; set; }
+    }
+
+    public enum OrderStatus
+    {
+        Preparing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
